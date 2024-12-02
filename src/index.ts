ee19ee458115627b97f1ef85b2e286edb8a532fd
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-12-02 17:27:39 +0100

    chore: add semantic release

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import recommendedConfig from './configs/recommended';
 import angularRecommendedConfig from './configs/angular-recommended';
 
-const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+const pkg = JSON.parse(fs.readFileSync("../package.json", "utf8"));
 
 const plugin = {
   meta: {
